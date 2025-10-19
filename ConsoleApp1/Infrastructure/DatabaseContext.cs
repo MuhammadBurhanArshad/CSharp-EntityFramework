856398968cb3957a9f199bcfadcab9f6fa3ec33d
MuhammadBurhanArshad <burhantheschoolboy@gmail.com>
2025-10-19 20:43:34 +0500

    App

---
+++ ConsoleApp1/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {       
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+.ConfigureServices((context, services) =>
+{
+    services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer("Data Source=(local); Initial Catalog=TechNova-ECommerce-A;Integrated Security=True;Encrypt=False")
+    );
+})
+.Build();
+
+
+//Data Source=(local); Initial Catalog=TechNova-ECommerce-A;Integrated Security=True;Encrypt=False
